6da1f5a45908e3e1729a97b1fdbb301c816974da
Youssef <ys1465@fayoum.edu.eg>
2025-11-10 17:49:37 +0200

    finish bills ui

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:smart_city/features/Auth/presentation/views/login_view.dart';
 import 'package:smart_city/features/Auth/presentation/views/signup_view.dart';
 import 'package:smart_city/features/Home/presentation/views/home_view.dart';
+import 'package:smart_city/features/bills/presentation/views/bill_details_view.dart';
+import 'package:smart_city/features/bills/presentation/views/bills_view.dart';
 import 'package:smart_city/features/notification/presentation/views/notification_view.dart';
 import 'package:smart_city/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:smart_city/features/payment/presentation/views/payment_confirmation_view.dart';
@@ -32,6 +34,10 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const homeView());
     case NotificationView.routeName:
       return MaterialPageRoute(builder: (context) => const NotificationView());
+    case BillsView.routeName:
+      return MaterialPageRoute(builder: (context) => const BillsView());
+    case BillDetailsView.routeName:
+      return MaterialPageRoute(builder: (context) => const BillDetailsView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
