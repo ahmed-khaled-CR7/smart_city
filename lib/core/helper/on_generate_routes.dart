3ecf4273b97ed1ee38962d283f4a4a2a4848a1e0
Youssef <ys1465@fayoum.edu.eg>
2025-11-10 18:12:39 +0200

    finish complaints screen ui

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -4,6 +4,7 @@ import 'package:smart_city/features/Auth/presentation/views/signup_view.dart';
 import 'package:smart_city/features/Home/presentation/views/home_view.dart';
 import 'package:smart_city/features/bills/presentation/views/bill_details_view.dart';
 import 'package:smart_city/features/bills/presentation/views/bills_view.dart';
+import 'package:smart_city/features/complaints/presentation/views/complaints_view.dart';
 import 'package:smart_city/features/notification/presentation/views/notification_view.dart';
 import 'package:smart_city/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:smart_city/features/payment/presentation/views/payment_confirmation_view.dart';
@@ -38,6 +39,8 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const BillsView());
     case BillDetailsView.routeName:
       return MaterialPageRoute(builder: (context) => const BillDetailsView());
+    case ComplaintsView.routeName:
+      return MaterialPageRoute(builder: (context) => const ComplaintsView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
