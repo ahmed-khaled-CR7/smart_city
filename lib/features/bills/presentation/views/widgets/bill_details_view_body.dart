445fe7bf645dffc6a3c7536a257d887c9daeb5dc
hagar <hajarghonim19@gmail.com>
2025-11-11 07:08:31 +0200

    enhance ui

--- lib/features/bills/presentation/views/widgets/bill_details_view_body.dart
+++ lib/features/bills/presentation/views/widgets/bill_details_view_body.dart
@@ -9,38 +9,85 @@ class BillDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 15.h),
-          const LabelValueRow(label: 'Service Type', value: 'Electricity'),
-          SizedBox(height: 10.h),
-          const LabelValueRow(label: 'Issue Date', value: '24/08/2024'),
-          Divider(height: 25.h, color: Colors.grey.shade300),
-          const LabelValueRow(
-            label: 'Total Amount',
-            value: 'EGP 350.00',
-            isAmount: true,
-          ),
-          Divider(height: 25.h, color: Colors.grey.shade300),
-          const LabelValueRow(label: 'status', value: 'Paid'),
-          const Spacer(),
-          CustomButton(
-            text: 'Pay Now',
-            color: AppColors.primaryColor,
-            textColor: Colors.black,
-            onPressed: () {},
-          ),
-          SizedBox(height: 15.h),
-          CustomButton(
-            text: 'Report a Problem',
-            color: AppColors.lightprimaryColor,
-            textColor: AppColors.secondaryColor,
-            onPressed: () {},
-          ),
-        ],
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 12),
+            Card(
+              color: Colors.white,
+              elevation: 4,
+              shadowColor: Colors.black12,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.r),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 18.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const LabelValueRow(
+                      label: 'Service Type',
+                      value: 'Electricity',
+                    ),
+                    SizedBox(height: 10.h),
+                    const LabelValueRow(
+                      label: 'Issue Date',
+                      value: '24/08/2024',
+                    ),
+                    Divider(height: 25.h, color: Colors.grey.shade300),
+                    const LabelValueRow(
+                      label: 'Total Amount',
+                      value: 'EGP 350.00',
+                      isAmount: true,
+                    ),
+                    Divider(height: 25.h, color: Colors.grey.shade300),
+                    const LabelValueRow(label: 'Status', value: 'Paid'),
+                  ],
+                ),
+              ),
+            ),
+
+            SizedBox(height: 35.h),
+            AnimatedOpacity(
+              opacity: 1,
+              duration: const Duration(milliseconds: 600),
+              child: Column(
+                children: [
+                  CustomButton(
+                    text: 'Pay Now',
+                    color: AppColors.primaryColor,
+                    textColor: Colors.black,
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Redirecting to payment...'),
+                          backgroundColor: Colors.green,
+                        ),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 15.h),
+                  CustomButton(
+                    text: 'Report a Problem',
+                    color: AppColors.lightprimaryColor,
+                    textColor: AppColors.secondaryColor,
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Opening complaint form...'),
+                          backgroundColor: Colors.orangeAccent,
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
